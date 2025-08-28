681f22932f48dd470e66df48205eb4c3bb2653f6
sunflowersjuan <sebasjuan331@gmail.com>
2025-08-28 18:35:51 -0500

    minimal changes in ExponentialDistribution

--- distributions/ExponentialDistribution.py
+++ distributions/ExponentialDistribution.py
@@ -11,10 +11,13 @@ class ExponentialDistribution:
         self.rate = rate
         self.n = n
         self.seed = seed
+        # generador de Ri congruencial lineal con parametros para generar minimo 1 millon de numeros
         self.lcg = LinealCongruence(xo_seed=self.seed,k=551757622, c=12345, g=31)  # Ejemplo de inicialización
 
 
+    #Genera los numeros Ni bajo una distribucion exponencial
     def generate_exponential(self):
         sequence = self.lcg.generate_sequence(self.n)
+        #secuencia de numeros exponenciales con la formula de transformacion inversa
         exponential_sequence = [- (1 / self.rate) * math.log(1 - u) for u in sequence]
         return exponential_sequence
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from generators.congruences.Congruences import LinealCongruence
 
-lcg = LinealCongruence(xo_seed=1,k=1, c=3, g=31)  # Ejemplo de inicialización
+lcg = LinealCongruence(xo_seed=1,k=1, c=3, g=60)  # Ejemplo de inicialización
 
 lcg.a=1103515245
 lcg.c=12345
@@ -8,6 +8,6 @@ lcg.c=12345
 
 print("Hull-Dobell Validation:", lcg.hull_dobell_validation())
 
-n = 10000
+n = 10000000
 sequence = lcg.generate_sequence(n)
 print(f"Generated sequence of {n} numbers:", sequence)
